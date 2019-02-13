fe24cd9f098152efbad5597df7170f3d82c15725
Lucas Eduardo da Silva <slucaseduardo@outlook.com>
2019-02-13 20:10:12 -0200

    Graphing Perlin function

---
+++ NoisePlay/Assets/GraphNoise.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GraphNoise : MonoBehaviour {
+
+	float t = 0;
+    float inc = 0.01f;
+    
+	void Update () 
+	{
+        t += inc;
+        float n = Mathf.PerlinNoise(t,1);
+        Grapher.Log(n, "Perlin1", Color.yellow);    
+	}
+}
